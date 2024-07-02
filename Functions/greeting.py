4f4de5da67b592de5a2cde823ca75655f0620b41
Kam Ephrem <kamnoah88@gmail.com>
2024-07-02 11:06:20 +0100

    added greeting.py file

---
+++ Functions/greeting.py
@@ -0,0 +1,18 @@
+# def greet(name):
+#     hello = f"Hello there {name}!"
+#     print(hello)
+# greet("kam")
+
+# def area(width, length):
+#     area = width * length
+#     return area
+   
+# answer = area(5,6)
+# print(answer)
+
+def odd_even(number):
+    if number % 2 == 0:
+        print(f"{number} is an even number!")
+    else:
+        print(f"{number} is an odd number!")
+odd_even(12)
