5c54ce4f8255bfcd80b37922cc001812f0e26d66
Kam Ephrem <kamnoah88@gmail.com>
2024-07-03 17:34:55 +0100

    practiced import custom module calculator.py to main.py

---
+++ Functions/calculator.py
@@ -0,0 +1,29 @@
+# Addition
+def add(num1, num2):
+    matteo = num1 + num2
+    print("The Sum of the two numbers is: ", matteo)
+
+# Subtraction
+def subtract(num1, num2):
+    naomi = num1 - num2
+    print("The Subtraction of the two numbers is: ", naomi)
+
+# Multiplication
+def multiply(num1, num2):
+    sam = num1 * num2
+    print("The Muliplicationof the two numbers is: ", sam)
+
+# Division
+def divide(num1, num2):
+    
+    if num2 != 0:
+       kam = num1/num2
+       print("The division of the two numbers is: ", kam)
+    else:
+        print("you can't divide a number by Zero")
+
+
+
+
+
+    
---
+++ Functions/main.py
@@ -0,0 +1,5 @@
+import calculator
+c1 = calculator.add(5,3)
+c2 = calculator.subtract(4,8)
+c3 = calculator.multiply(4,8)
+c4 = calculator.divide(32,8)
