136b25132631f1437fe21dae8f229e2e59cfd61f
Kam Ephrem <kamnoah88@gmail.com>
2024-07-03 13:58:33 +0100

    added practice_data_structure.py

---
+++ Functions/practice_data_structure.py
@@ -0,0 +1,85 @@
+###### Lists
+# kams_list = [5,"hello",7,10]
+# kams_list_2 = [5,"hello",7,10]
+# kams_list.append(24)
+# del kams_list[2]
+# print(kams_list)
+# print(kams_list_2)
+#
+###### Tupels
+# a = ("hi",0,"pass",1,2,5)
+# print(a)
+# print(type(a))
+# a = list(a)
+# print(a)
+# print(a[2])
+# a.append(1000)
+# print(a)
+# print(type(a))
+# del a[2]
+# print(a)
+# b = {"Hello",5,6,2,5,5,5}
+# print(b)
+# print(type(b))
+#
+###### SETS
+# a = {3,4,5}
+# b={4,5,6,8,6,7,9,8,8}
+# print(b)
+# c= b.intersection(a)
+# print(c)
+# d = b.union(a)
+# print (d)
+# f = 3
+# print(f in a)
+# print(f in b)
+# g = a.difference(b)
+# print(g)
+# h = b.difference(a)
+# print(h)
+#
+###### Dictionaries....
+#key value pairs
+#the key gives the ability to index your sets
+# ydict = {'k1': 'v1', 'k2': 'v2', 'k3': 'v3', 'k4': 'v4'}
+# print(ydict)
+
+# xdict = dict([('k1','v1'), ('k2','v2'), ('k3', 'v3'), ('k4', 'v4')])
+# print(xdict)
+
+# print(xdict['k3'])
+# Methods in Dictonaries
+
+# ydict = dict([(1,1), (2,3), (3,3)])
+# print(ydict)
+
+# xdict = dict([('k1', 'v1'), ('k2', 'v2'), ('k3','v3')])
+# print(xdict)
+# ydict.update(xdict)
+# print(ydict)
+# xdict.update(ydict)
+# print(xdict)
+#
+# z = dict()
+# z['a'] = 'alpha'
+# z['g'] = 'gama'
+# z['o'] = 'omega'
+
+#{'a': 'alpha', 'g': 'gama', 'o': 'omega'}
+# print(z)
+# print(set(z))
+##{'o', 'a', 'g'}
+# When changed to a set it only prints the keys and not the values
+
+def funcd(d, v):
+    for key in d:
+        if d[key] == v:
+            return key
+    return dict()
+
+ydict = dict([('v1', 1), ('v2', 2), ('v3', 3)])
+print(ydict)
+
+
+print(funcd(ydict, 2))
+print(funcd(ydict, 7))
