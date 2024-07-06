31bd6ab52da885d923890e558cd0fe9cf0d573f0
Kam Ephrem <kamnoah88@gmail.com>
2024-07-06 16:43:11 +0100

    updated greeting.py with the latest changes

--- Functions/greeting.py
+++ Functions/greeting.py
@@ -16,3 +16,19 @@
 #     else:
 #         print(f"{number} is an odd number!")
 # odd_even(12)
+
+# Practicing Namespace
+# x = 'global x'
+# def outer():
+#     x = 'outer x'
+#     def inner():
+#         x = 'inner x'
+#         print(x)
+#     inner()     
+#     print(x)
+
+# outer()    # in the above function python follows the LEGB rule first, when the functions are executed first it looks for the variable x in the local scope then it looks for it in the Eclosing scope if it can't find it, finally it will look for it in the Global Scope.
+# import math
+
+# s = math.sqrt(4)
+# print(s)
